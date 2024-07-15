844305ca70984a595df53dfb61f124cd9c324a92
devhatebug <togiap.dev@gmail.com>
2024-07-15 16:10:04 +0700

    update

--- client/src/app/register/page.js
+++ client/src/app/register/page.js
@@ -36,7 +36,10 @@ const SignUpPage = () => {
                   Tên hiển thị
                 </label>
                 <div className="mt-1 relative rounded-md shadow-sm">
-                  <input id="name" className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                  <input
+                    id="name"
+                    className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md placeholder-gray-400 focus:outline-none focus:shadow-outline-blue focus:border-blue-300 transition duration-150 ease-in-out sm:text-sm sm:leading-5"
+                  />
                   <div className="hidden absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
                     <svg
                       className="h-5 w-5 text-red-500"
